f76d55dac3f2385c0a035807385e6e01f8a83830
soyoungxxx <otherplet@gmail.com>
2023-11-21 15:58:07 +0900

    edit

--- detect.py
+++ detect.py
@@ -1,86 +1,53 @@
 import os
-import sys
-from pathlib import Path
+import cv2
 
 import torch
 import json
-import numpy as np
-import glob
-
-FILE = Path(__file__).resolve()
-ROOT = FILE.parents[0]  # YOLOv5 root directory
-if str(ROOT) not in sys.path:
-    sys.path.append(str(ROOT))  # add ROOT to PATH
-ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
-# 경로 설정
 
 from ultralytics.utils.plotting import Annotator, colors
 
-from models.common import DetectMultiBackend
-from utils.general import (Profile, check_img_size, cv2, check_version,
-                           non_max_suppression, scale_boxes, xyxy2xywh)
-from utils.torch_utils import select_device
+from dataloader import LoadImages;
+from common import DetectMultiBackend
+
+from general import (Profile, check_img_size, check_version,
+                    non_max_suppression, scale_boxes)
 #필요한 코드 import
 
-class LoadImages:
-    def __init__(self, path, img_size=640, stride=32, auto=True, transforms=None, vid_stride=1):
-        if isinstance(path, str) and Path(path).suffix == '.txt':  # *.txt file with img/vid/dir on each line
-            path = Path(path).read_text().rsplit()
-        files = []
-        for p in sorted(path) if isinstance(path, (list, tuple)) else [path]:
-            p = str(Path(p).resolve())
-            if '*' in p:
-                files.extend(sorted(glob.glob(p, recursive=True)))  # glob
-            elif os.path.isdir(p):
-                files.extend(sorted(glob.glob(os.path.join(p, '*.*'))))  # dir
-            elif os.path.isfile(p):
-                files.append(p)  # files
-            else:
-                raise FileNotFoundError(f'{p} does not exist')
+def select_device(device='', batch_size=0, newline=True):
+    # device = None or 'cpu' or 0 or '0' or '0,1,2,3'
+    s = ''
+    device = str(device).strip().lower().replace('cuda:', '').replace('none', '')  # to string, 'cuda:0' to '0'
+    cpu = device == 'cpu'
+    mps = device == 'mps'  # Apple Metal Performance Shaders (MPS)
+    if cpu or mps:
+        os.environ['CUDA_VISIBLE_DEVICES'] = '-1'  # force torch.cuda.is_available() = False
+    elif device:  # non-cpu device requested
+        os.environ['CUDA_VISIBLE_DEVICES'] = device  # set environment variable - must be before assert is_available()
+        assert torch.cuda.is_available() and torch.cuda.device_count() >= len(device.replace(',', '')), \
+            f"Invalid CUDA '--device {device}' requested, use '--device cpu' or pass valid CUDA device(s)"
 
-        images = [x for x in files if x.split('.')[-1].lower() in IMG_FORMATS]
-        ni = len(images)
+    if not cpu and not mps and torch.cuda.is_available():  # prefer GPU if available
+        devices = device.split(',') if device else '0'  # range(torch.cuda.device_count())  # i.e. 0,1,6,7
+        n = len(devices)  # device count
+        if n > 1 and batch_size > 0:  # check batch_size is divisible by device_count
+            assert batch_size % n == 0, f'batch-size {batch_size} not multiple of GPU count {n}'
+        space = ' ' * (len(s) + 1)
+        for i, d in enumerate(devices):
+            p = torch.cuda.get_device_properties(i)
+            s += f"{'' if i == 0 else space}CUDA:{d} ({p.name}, {p.total_memory / (1 << 20):.0f}MiB)\n"  # bytes to MB
+        arg = 'cuda:0'
+    elif mps and getattr(torch, 'has_mps', False) and torch.backends.mps.is_available():  # prefer MPS if available
+        s += 'MPS\n'
+        arg = 'mps'
+    else:  # revert to CPU
+        s += 'CPU\n'
+        arg = 'cpu'
 
-        self.img_size = img_size
-        self.stride = stride
-        self.files = images + videos
-        self.nf = ni + nv  # number of files
-        self.video_flag = [False] * ni + [True] * nv
-        self.mode = 'image'
-        self.auto = auto
-        self.transforms = transforms  # optional
-        self.vid_stride = vid_stride  # video frame-rate stride
-        self.cap = None
-
-    def __iter__(self):
-        self.count = 0
-        return self
-
-    def __next__(self):
-        if self.count == self.nf:
-            raise StopIteration
-        path = self.files[self.count]
-
-        # Read image
-        self.count += 1
-        im0 = cv2.imread(path)  # BGR
-        assert im0 is not None, f'Image Not Found {path}'
-        s = f'image {self.count}/{self.nf} {path}: '
-
-        if self.transforms:
-            im = self.transforms(im0)  # transforms
-        else:
-            im = letterbox(im0, self.img_size, stride=self.stride, auto=self.auto)[0]  # padded resize
-            im = im.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
-            im = np.ascontiguousarray(im)  # contiguous
-
-        return path, im, im0, self.cap, s
-
-    def __len__(self):
-        return self.nf  # number of files
+    if not newline:
+        s = s.rstrip()
+    return torch.device(arg)
 
 def smart_inference_mode(torch_1_9=check_version(torch.__version__, '1.9.0')):
-    # Applies torch.inference_mode() decorator if torch>=1.9.0 else torch.no_grad() decorator
     def decorate(fn):
         return (torch.inference_mode if torch_1_9 else torch.no_grad)()(fn)
 
@@ -88,39 +55,28 @@ def smart_inference_mode(torch_1_9=check_version(torch.__version__, '1.9.0')):
 
 @smart_inference_mode()
 def run(
-        weights=ROOT / 'best.pt',  # model path or triton URL
-        source=ROOT / 'data/images',  # file/dir/URL/glob/screen/0(webcam)
-        data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
-        imgsz=(640, 640),  # inference size (height, width)
-        conf_thres=0.25,  # confidence threshold
-        iou_thres=0.45,  # NMS IOU threshold
-        max_det=1000,  # maximum detections per image
-        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-        save_conf=False,  # save confidences in --save-txt labels
-        classes=None,  # filter by class: --class 0, or --class 0 2 3
-        agnostic_nms=False,  # class-agnostic NMS
-        line_thickness=3,  # bounding box thickness (pixels)
-        hide_labels=False,  # hide labels
-        hide_conf=False,  # hide confidences
-        vid_stride=1,  # video frame-rate stride
+        weights='best.pt',
+        img = ['img1.jpg', 'img2.jpg'],
+        data='custom_dataset.yaml',
+        imgsz=(640, 640)
 ):
     class_dict = dict()
 
+#def run(yamlfile, ptfile, img, imgsz)
+
     # Load model
-    # data = yaml file
-    # weights = pt file
-    device = select_device(device)
+    device = select_device()
     model = DetectMultiBackend(weights, device=device, dnn=False, data=data, fp16=False)
     stride, names, pt = model.stride, model.names, model.pt
     imgsz = check_img_size(imgsz, s=stride)  # check image size
 
     # Dataloader
-    dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
+    dataset = LoadImages(img, img_size=imgsz, stride=stride, auto=pt, vid_stride=1)
 
     # Run inference
     model.warmup(imgsz=(1, 3, *imgsz))  # warmup
     seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
-    for path, im, im0s, vid_cap, s in dataset:
+    for im, im0s in dataset:
         with dt[0]:
             im = torch.from_numpy(im).to(model.device)
             im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
@@ -134,16 +90,15 @@ def run(
 
         # NMS
         with dt[2]:
-            pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+            pred = non_max_suppression(pred, conf_thres=0.25, iou_thres=0.45, classes=None, agnostic_nms=False, max_det=1000)
+            # pred = list of detections, on (n,6) tensor per image [xyxy, conf, cls]
 
         # Process predictions
         for i, det in enumerate(pred):  # per image
             seen += 1
-            p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
+            im0, frame = im0s.copy(), getattr(dataset, 'frame', 0)
 
-            p = Path(p)  # to Path
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-            annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+            annotator = Annotator(im0, line_width=3, example=str(names))
             if len(det):
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
@@ -151,12 +106,10 @@ def run(
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
                     c = int(cls)  # integer class
-                    label = names[c] if hide_conf else f'{names[c]}'
-                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                    line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
+                    label = names[c]
 
                     # custom codes
-                    key = names[int(line[0])]
+                    key = names[c]
                     if (key in class_dict) :
                         value = class_dict.get(key) + 1
                         class_dict.update({key : value})
@@ -165,16 +118,13 @@ def run(
                     # class 별로 객체 세는 코드
 
                     c = int(cls)  # integer class
-                    label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
+                    label = names[c]
                     annotator.box_label(xyxy, label, color=colors(c, True))
                     # img에 bbox 추가하는 코드
 
-            # Save results (image with detections)
-            if save_img:
-                cv2.imwrite(save_path, im0)
+            cv2.imwrite(save_path, im0)
             # img 저장하는 부분 -> return으로 변경 필요
 
-    # --------------------------------------------------
     with open(f'{save_dir}/data.json', 'a') as f:
         json.dump(class_dict, f, ensure_ascii=False, indent=4)
     # ---------- 클래스 별로 객체 개수 json 저장---------
